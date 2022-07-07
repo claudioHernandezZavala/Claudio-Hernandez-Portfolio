c43b905bf3ac6256279fe8885239f77cc4bcbbdc
Claudio Andre Hernandez Zavala <claudiohernandez.zavala@gmail.com>
2022-07-06 20:54:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var p =  document.createElement("img");
+p.src = './github.png';
+var element = document.getElementById("github");
+element.appendChild(p);
+
+// const p2 =  document.createElement("p");
+// const text =  document.createTextNode("heeey");
+// p2.appendChild(text);
+// const element = document.getElementById("github").appendChild(p2);
+// element.appendChild(p2);
